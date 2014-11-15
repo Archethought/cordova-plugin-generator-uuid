84889ddf522e7240a1e6d2b17c8900a52af84146
Daniel P Williams <dwilliams@port8080.net>
2014-11-15 12:42:00 -0700

    Made a plugin.

---
+++ src/android/uuid.java
@@ -0,0 +1,82 @@
+/*** This is the Android Java Source File ***/
+package com.archethought.cordovaplugins.generators;
+
+import org.apache.cordova.*;
+
+import android.content.Context;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.util.UUID;
+
+/***
+ * This class called by CordovaActivity to store strings in a circular buffer.
+ ***/
+public class uuid extends CordovaPlugin {
+    /***
+     * Class Variables
+     ***/
+    // private int max;
+    
+    /*** Cordova Magic Functions ***/
+    
+    @Override
+    public void initialize( final CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        
+        // Initialization of the goodies.
+        // this.max = 300;
+    }
+    
+    @Override
+    public boolean execute( String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        // These if statements call the actual functions.  There must be one for each public function.
+        // randomUUID function
+        if( "randomUUID".equals( action)) {
+            this.pop( callbackContext);
+            return true;
+        }
+        
+        // This is needed to be PC ( programmatically correct).
+        return false;
+    }
+    
+    // Make another constructor here that allows a number of items to be set.
+    
+    /*** PUBLIC FUNCTIONS ***/
+    
+    /***
+     * randomUUID function
+     *   Generate a random UUID and return a JSONObject containg a string representing the UUID.
+     ***/
+    public void randomUUID( CallbackContext callbackContext) {
+        // Create a random UUID
+        UUID generatedUUID = UUID.randomUUID();
+        // Build the JSONObject for return
+        JSONObject generatedJSON = this.jsonify( generatedUUID);
+        // Return the JSONObject
+        callbackContext.success( generatedJSON);
+        return;
+    }
+    
+    /*** PRIVATE FUNCTIONS ***/
+    
+    /***
+     * jsonify function
+     *   Return a JSONObject containing the UUID information
+     ***/
+    private boolean jsonify( UUID inputUUID) {
+        // Create a JSONObject
+        JSONObject outputJSON = new JSONObject();
+        // Copy the needed values into the JSONObject
+        outputJSON.put( "string", inputUUID.toString());
+        outputJSON.put( "mostSignificantBits", inputUUID.getMostSignificantBits());
+        outputJSON.put( "leastSignificantBits", inputUUID.getLeastSignificantBits());
+        outputJSON.put( "version", inputUUID.version());
+        outputJSON.put( "variant", inputUUID.variant());
+        // Return the JSONObject
+        return( outputJSON);
+    }
+}
---
+++ www/generator_uuid.js
@@ -0,0 +1,26 @@
+/*** This is the javascript interface that the app uses. ***/
+//
+// Use jsdoc to generate documentation.
+
+// The following line causes a jsdoc error.
+// Use the jsdoc option -l to ignore the error.
+var exec = cordova.require('cordova/exec');
+
+/** @module com.archethought.cordovaplugins.generators.uuid */
+
+/** Generate a random UUID ( type 4)
+* @param {resultCallback} resultCallback - The function that will handle the result.
+* @example
+generator_uuid.randomUUID(
+    function( uuid) {
+        console.log('Random UUID String: ' + uuid.string);
+        console.log('Random UUID Most Significant Bits: ' + uuid.mostSignificantBits);
+        console.log('Random UUID Least Significant Bits: ' + uuid.leastSignificantBits);
+        console.log('Random UUID Version Number: ' + uuid.version);
+        console.log('Random UUID Variant Number: ' + uuid.variant);
+    }
+);
+*/
+exports.randomUUID = function( resultCallback) {
+    exec( resultCallback, null, 'generator_uuid', 'randomUUID', []);
+};
